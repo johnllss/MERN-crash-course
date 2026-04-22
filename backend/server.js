7ddb35b910f590886d37e9d2005a3b26d08b67b3
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2026-04-22 15:20:43 +0800

    add: get all prods, delete a prod

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,21 @@ dotenv.config();
 
 const app = express();
 
+// middleware (process that runs before sending a response back to client)
+app.use(express.json()); // allow us to accept JSON data in request body
+
+// get all products
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({}); // empty object means find all products
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.log("Error in fetching products:", error.message);
+        res.status(500).json({ success: false, message: "Server Error." });
+    }
+});
+
+// create product
 app.post("/api/products", async (req, res) => {
     const product = req.body;
 
@@ -21,6 +36,23 @@ app.post("/api/products", async (req, res) => {
         await newProduct.save();
         res.status(201).json({ success: true, data: newProduct });
     } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error." });
+    }
+});
+
+// delete a product
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ success: false, message: "Invalid product ID." });
+    }
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted." });
+    } catch (error) {
+        console.log("Error in deleting product:", error.message);
         res.status(500).json({ success: false, message: "Server Error." });
     }
 });
