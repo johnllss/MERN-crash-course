9cd9912eb85dc3d58638d1e182dbd87f0caa411b
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2026-04-27 12:16:40 +0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
 import productRoutes from './routes/product.route.js';
 
 dotenv.config();
@@ -16,7 +14,13 @@ app.use(express.json()); // allow us to accept JSON data in request body
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server started at http://localhost:${PORT}`);
-});
+
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server started at http://localhost:${PORT}`);
+    });
+};
+
+startServer();
