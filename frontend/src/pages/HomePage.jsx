2f68c4a14cc2eb04b0c93c2b57f70ff050a97119
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2026-04-27 12:02:31 +0800

    feat: product card initial

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
-import ProductCard from '@/components/ProductCard';
 import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react';
-import React from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useProductStore } from '@/store/product';
+import ProductCard from '@/components/ProductCard';
 
 const HomePage = () => {
   const { fetchProducts, products } = useProductStore();
@@ -30,7 +31,7 @@ const HomePage = () => {
             md: 2,
             lg: 3
           }}
-          spacing={10}
+          gap={10}
           w={"full"}
         >
           {products.map((product) => (
@@ -38,14 +39,16 @@ const HomePage = () => {
           ))}
         </SimpleGrid>
 
-        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-          No products found {" "}
-          <Link to={"/create"}>
-            <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
-              Create a product
-            </Text>
-          </Link>
-        </Text>
+        {products.length === 0 && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+            No products found {" "}
+            <Link to={"/create"}>
+              <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
       </VStack>
     </Container>
   );
